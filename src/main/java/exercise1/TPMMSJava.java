f8312025e46a8f36ec981e1bce11ca1f83716dd3
cdfhalle <conradhalle@gmail.com>
2022-11-09 16:59:02 +0100

    Finished exercise 1 ...naja fast

--- src/main/java/exercise1/TPMMSJava.java
+++ src/main/java/exercise1/TPMMSJava.java
@@ -1,18 +1,22 @@
 package exercise1;
 
 import de.hpi.dbs2.ChosenImplementation;
+import de.hpi.dbs2.dbms.Block;
 import de.hpi.dbs2.dbms.BlockManager;
 import de.hpi.dbs2.dbms.BlockOutput;
 import de.hpi.dbs2.dbms.Relation;
 import de.hpi.dbs2.exercise1.SortOperation;
 import org.jetbrains.annotations.NotNull;
 
-@ChosenImplementation(false)
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+
+@ChosenImplementation(true)
 public class TPMMSJava extends SortOperation {
     public TPMMSJava(@NotNull BlockManager manager, int sortColumnIndex) {
         super(manager, sortColumnIndex);
     }
-
     @Override
     public int estimatedIOCost(@NotNull Relation relation) {
         throw new UnsupportedOperationException("TODO");
