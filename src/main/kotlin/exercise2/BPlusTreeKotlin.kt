d93a00209deee6e5a4b98b18227e8ce84f2ee43a
conradhalle <conradhalle@gmail.com>
2022-12-09 20:36:02 +0100

    Changes by johanna

--- src/main/kotlin/exercise2/BPlusTreeKotlin.kt
+++ src/main/kotlin/exercise2/BPlusTreeKotlin.kt
@@ -11,12 +11,12 @@ class BPlusTreeKotlin : AbstractBPlusTree {
     constructor(order: Int) : super(order)
     constructor(rootNode: BPlusTreeNode<*>) : super(rootNode)
 
-    private fun findLeafPath(key: Int): Stack<BPlusTreeNode<*>>{
-        var currentNode : BPlusTreeNode<*> = rootNode
+    private fun findLeafPath(key: Int): Stack<BPlusTreeNode<*>> {
+        var currentNode: BPlusTreeNode<*> = rootNode
         val path = Stack<BPlusTreeNode<*>>()
         path.push(currentNode)
 
-        while(currentNode.height != 0){
+        while (currentNode.height != 0) {
             currentNode = (currentNode as InnerNode).selectChild(key)
             path.push(currentNode)
         }
@@ -222,26 +222,27 @@ class BPlusTreeKotlin : AbstractBPlusTree {
         }
     }
 
-        // Find LeafNode in which the key has to be inserted.
-        //   It is a good idea to track the "path" to the LeafNode in a Stack or something alike.
-        // Does the key already exist? Overwrite!
-        //   leafNode.references[pos] = value;
-        //   But remember return the old value!
-        // New key - Is there still space?
-        //   leafNode.keys[pos] = key;
-        //   leafNode.references[pos] = value;
-        //   Don't forget to update the parent keys and so on...
-        // Otherwise
-        //   Split the LeafNode in two!
-        //   Is parent node root?
-        //     update rootNode = ... // will have only one key
-        //   Was node instanceof LeafNode?
-        //     update parentNode.keys[?] = ...
-        //   Don't forget to update the parent keys and so on...
+    // Find LeafNode in which the key has to be inserted.
+    //   It is a good idea to track the "path" to the LeafNode in a Stack or something alike.
+    // Does the key already exist? Overwrite!
+    //   leafNode.references[pos] = value;
+    //   But remember return the old value!
+    // New key - Is there still space?
+    //   leafNode.keys[pos] = key;
+    //   leafNode.references[pos] = value;
+    //   Don't forget to update the parent keys and so on...
+    // Otherwise
+    //   Split the LeafNode in two!
+    //   Is parent node root?
+    //     update rootNode = ... // will have only one key
+    //   Was node instanceof LeafNode?
+    //     update parentNode.keys[?] = ...
+    //   Don't forget to update the parent keys and so on...
 
-        // Check out the exercise slides for a flow chart of this logic.
-        // If you feel stuck, try to draw what you want to do and
-        // check out Ex2Main for playing around with the tree by e.g. printing or debugging it.
-        // Also check out all the methods on BPlusTreeNode and how they are implemented or
-        // the tests in BPlusTreeNodeTests and BPlusTreeTests!
+    // Check out the exercise slides for a flow chart of this logic.
+    // If you feel stuck, try to draw what you want to do and
+    // check out Ex2Main for playing around with the tree by e.g. printing or debugging it.
+    // Also check out all the methods on BPlusTreeNode and how they are implemented or
+    // the tests in BPlusTreeNodeTests and BPlusTreeTests!
+
 }
