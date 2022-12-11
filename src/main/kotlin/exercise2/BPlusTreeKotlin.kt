b38c7eeb3a6d6f924022992e9c594053aa6b63b3
joh-dah <johanna@dahlkemper.de>
2022-12-11 20:41:44 +0100

    Fixed bugs

--- src/main/kotlin/exercise2/BPlusTreeKotlin.kt
+++ src/main/kotlin/exercise2/BPlusTreeKotlin.kt
@@ -2,7 +2,6 @@ package exercise2
 
 import de.hpi.dbs2.ChosenImplementation
 import de.hpi.dbs2.exercise2.*
-import java.util.Random
 import java.util.Stack
 import kotlin.math.ceil
 
@@ -22,15 +21,17 @@ class BPlusTreeKotlin : AbstractBPlusTree {
         }
         return path
     }
+
+
     private inline fun <reified T> insertKeyAndReference(
         key: Int,
         reference: T,
         keys: Array<Int?>,
         references: Array<T?>
     ): Pair<Array<Int?>, Array<T?>> {
-        val newKeys = Array<Int?>(keys.size +1) { null }
-        val newReferences = Array<T?>(references.size +1) { null }
-        var position = 0;
+        val newKeys = Array<Int?>(keys.size + 1) { null }
+        val newReferences = Array<T?>(references.size + 1) { null }
+        var position = 0
         val offset = if (reference is ValueReference) 0 else 1
         while (position < keys.size && keys[position] != null && keys[position]!! < key) {
             position++
@@ -43,24 +44,30 @@ class BPlusTreeKotlin : AbstractBPlusTree {
         references.copyInto(newReferences, position + 1 + offset, position + offset, references.size)
         return Pair(newKeys, newReferences)
     }
+
+
     private fun LeafNode.insertKeyAndReference(key: Int, reference: ValueReference) {
         val result = insertKeyAndReference(key, reference, this.keys, this.references)
-        for (i in 0 until this.keys.size){
+        for (i in 0 until this.keys.size) {
             this.keys[i] = result.first[i]
         }
         for (i in 0 until this.references.size) {
             this.references[i] = result.second[i]
         }
     }
+
+
     private fun InnerNode.insertKeyAndReference(key: Int, reference: BPlusTreeNode<*>) {
         val result = insertKeyAndReference(key, reference, this.keys, this.references)
-        for (i in 0 until this.keys.size){
+        for (i in 0 until this.keys.size) {
             this.keys[i] = result.first[i]
         }
         for (i in 0 until this.references.size) {
             this.references[i] = result.second[i]
         }
     }
+
+
     private fun LeafNode.insertAndSplit(key: Int, reference: ValueReference): LeafNode {
         // insert key and reference
         val (newKeys, newReferences) = insertKeyAndReference<ValueReference>(key, reference, this.keys, this.references)
@@ -77,9 +84,15 @@ class BPlusTreeKotlin : AbstractBPlusTree {
         this.nextSibling = rightNode
         return rightNode
     }
+
     private fun InnerNode.insertAndSplit(key: Int, reference: BPlusTreeNode<*>): InnerNode {
         // insert key and reference
-        val (newKeys, newReferences) = insertKeyAndReference<BPlusTreeNode<*>>(key, reference, this.keys, this.references)
+        val (newKeys, newReferences) = insertKeyAndReference<BPlusTreeNode<*>>(
+            key,
+            reference,
+            this.keys,
+            this.references
+        )
         // move half of the keys and references to a new LeafNode
         val divider = ceil((this.n + 1) / 2.0).toInt()
         newKeys.copyInto(this.keys, 0, 0, divider)
@@ -91,22 +104,25 @@ class BPlusTreeKotlin : AbstractBPlusTree {
         newReferences.copyInto(rightNode.references, 0, divider, newReferences.size)
         return rightNode
     }
+
     private fun createNewRootNode(leftNode: BPlusTreeNode<*>, rightNode: BPlusTreeNode<*>): InnerNode {
         val newRootNode = InnerNode(this.order)
         if (leftNode is LeafNode) {
             newRootNode.keys[0] = rightNode.keys[0]
         } else {
-            newRootNode.keys[0] = leftNode.keys.last()
-            leftNode.keys[leftNode.keys.lastIndex] = null
+            val keyPosition = ceil((leftNode.n + 1) / 2.0).toInt() - 1
+            newRootNode.keys[0] = leftNode.keys[keyPosition]
+            leftNode.keys[keyPosition] = null
         }
         newRootNode.references[0] = leftNode
         newRootNode.references[1] = rightNode
         return newRootNode
     }
+
     override fun insert(key: Int, value: ValueReference): ValueReference? {
 
         val path = findLeafPath(key)
-        var leaf = path.pop() as LeafNode
+        val leaf = path.pop() as LeafNode
         var returnValue: ValueReference? = null
         // key already exists
         if (leaf.getOrNull(key) != null) {
@@ -118,7 +134,7 @@ class BPlusTreeKotlin : AbstractBPlusTree {
             }
         } else {
             var currentNode: BPlusTreeNode<*> = leaf
-            var finished: Boolean = false
+            var finished = false
             var rightChild: BPlusTreeNode<*>
             var key = key
             var reference: BPlusTreeNode<*> = leaf
@@ -131,11 +147,20 @@ class BPlusTreeKotlin : AbstractBPlusTree {
                     finished = true
                 } else {
                     when (currentNode) {
+                        is InitialRootNode -> {
+                            val leftNode = LeafNode(order)
+                            currentNode.keys.copyInto(leftNode.keys)
+                            leftNode.references = currentNode.references
+                            val rightNode = leftNode.insertAndSplit(key, value)
+                            this.rootNode = createNewRootNode(leftNode, rightNode)
+                            leftNode.nextSibling = rightNode
+                            finished = true
+                        }
+
                         rootNode -> {
-                            val newNode = when (currentNode) {
-                                is LeafNode -> currentNode.insertAndSplit(key, value)
-                                is InnerNode -> currentNode.insertAndSplit(key, reference)
-                            }
+                            currentNode as InnerNode
+                            val newNode = currentNode.insertAndSplit(key, reference)
+
                             this.rootNode = createNewRootNode(currentNode, newNode)
                             finished = true
                         }
@@ -161,88 +186,4 @@ class BPlusTreeKotlin : AbstractBPlusTree {
 
         return returnValue
     }
-
-
-    private fun insertIntoLeaf(key: Int, value: ValueReference, leaf: LeafNode) {
-        val random = Random()
-        var result = BadinsertKeyAndReference(key, value, leaf.keys, leaf.references)
-        if (random.nextBoolean())
-            result = insertKeyAndReference(key, value, leaf.keys, leaf.references)
-        for (i in 0 until leaf.n) {
-            leaf.keys[i] = result.first[i]
-            leaf.references[i] = result.second[i]
-        }
-    }
-    private inline fun <reified T> BadinsertKeyAndReference(
-        key: Int,
-        reference: T,
-        keys: Array<Int?>,
-        references: Array<T?>
-    ): Pair<Array<Int?>, Array<T?>> {
-        var keyList = keys.toMutableList()
-        var referenceList = references.toMutableList()
-        var i = 0
-        while (keyList[i] != null && keyList[i]!! < key) {
-            i++
-        }
-        keyList.add(i, key)
-        referenceList.add(i+1, reference)
-        return Pair(keyList.toTypedArray<Int?>(), referenceList.toTypedArray<T?>())
-    }
-    private fun insertEntryIntoLeaf(key: Int, value: ValueReference, leaf: LeafNode, parent: InnerNode) {
-        // check if node is root node
-        // ...
-        // node is not full
-        if (!leaf.isFull) {
-            var result = insertKeyAndReference(key, value, leaf.keys, leaf.references)
-            for (i in 0 until leaf.n) {
-                leaf.keys[i] = result.first[i]
-                leaf.references[i] = result.second[i]
-            }
-        }
-        // node already full -> Split the LeafNode in two!
-        else {
-            // move half of the keys and references to a new LeafNode
-            val (newKeys, newReferences) = insertKeyAndReference(key, value, leaf.keys, leaf.references)
-            val divider = ceil((leaf.n + 1) / 2.0).toInt()
-            for (i in 0 until divider) {
-                leaf.keys[i] = newKeys[i]
-                leaf.references[i] = newReferences[i]
-            }
-            var rightNode = LeafNode(order)
-            for (i in divider until leaf.n) {
-                rightNode.keys[i] = newKeys[i]
-                rightNode.references[i] = newReferences[i]
-                leaf.keys[i] = null
-                leaf.references[i] = null
-            }
-            rightNode.nextSibling = leaf.nextSibling
-            leaf.nextSibling = rightNode
-            // insertEntryIntoInnerNode(leaf.getSmallestKey(), rightNode, parent)
-        }
-    }
-
-    // Find LeafNode in which the key has to be inserted.
-    //   It is a good idea to track the "path" to the LeafNode in a Stack or something alike.
-    // Does the key already exist? Overwrite!
-    //   leafNode.references[pos] = value;
-    //   But remember return the old value!
-    // New key - Is there still space?
-    //   leafNode.keys[pos] = key;
-    //   leafNode.references[pos] = value;
-    //   Don't forget to update the parent keys and so on...
-    // Otherwise
-    //   Split the LeafNode in two!
-    //   Is parent node root?
-    //     update rootNode = ... // will have only one key
-    //   Was node instanceof LeafNode?
-    //     update parentNode.keys[?] = ...
-    //   Don't forget to update the parent keys and so on...
-
-    // Check out the exercise slides for a flow chart of this logic.
-    // If you feel stuck, try to draw what you want to do and
-    // check out Ex2Main for playing around with the tree by e.g. printing or debugging it.
-    // Also check out all the methods on BPlusTreeNode and how they are implemented or
-    // the tests in BPlusTreeNodeTests and BPlusTreeTests!
-
 }
--- src/main/kotlin/exercise2/Ex2Main.kt
+++ src/main/kotlin/exercise2/Ex2Main.kt
@@ -1,37 +1,36 @@
 package exercise2
 
-import de.hpi.dbs2.exercise2.AbstractBPlusTree
-import de.hpi.dbs2.exercise2.BPlusTreeNode
 import de.hpi.dbs2.exercise2.*
 
 fun main() {
+/*
     val order = 4
 
     val root = BPlusTreeNode.buildTree(order,
         arrayOf(
             entryArrayOf(
-                2 to ref(0),
-                3 to ref(1),
-                5 to ref(2)
+                2 to ref(2),
+                3 to ref(3),
+                5 to ref(5)
             ), entryArrayOf(
-                7 to ref(3),
-                11 to ref(4)
+                7 to ref(7),
+                11 to ref(11)
             )
         ), arrayOf(
             entryArrayOf(
-                13 to ref(5),
-                17 to ref(6),
-                19 to ref(7),
+                13 to ref(13),
+                17 to ref(17),
+                19 to ref(19),
             ), entryArrayOf(
-                23 to ref(8),
-                29 to ref(9)
+                23 to ref(23),
+                29 to ref(29)
             ), entryArrayOf(
-                31 to ref(10),
-                37 to ref(11),
-                41 to ref(12)
+                31 to ref(31),
+                37 to ref(37),
+                41 to ref(41)
             ), entryArrayOf(
-                43 to ref(13),
-                47 to ref(14)
+                43 to ref(43),
+                47 to ref(47)
             )
         )
     )
@@ -40,28 +39,28 @@ fun main() {
     println(tree)
 
     // check what happens when we insert a key that already exists
-    // println(tree.insert(2, ref(15)))
-    // println(tree)
+    println(tree.insert(2, ref(15)))
+    println(tree)
 
     // check what happens when we insert a key that does not exist
-    println(tree.insert(8, ref(11)))
-    println(tree.insert(9, ref(12)))
-
-    println(tree.insert(10, ref(13)))
-
-    println(tree.insert(12, ref(14)))
+    tree.insert(8, ref(8))
     println(tree)
-    println(tree.insert(6, ref(15)))
+    tree.insert(9, ref(9))
     println(tree)
-
-
+    tree.insert(10, ref(10))
+    println(tree)
+    tree.insert(12, ref(12))
+    println(tree)
+    tree.insert(6, ref(6))
+    println(tree)
+*/
 
 
     /*
      * playground
      * ~ feel free to experiment with the tree and tree nodes here
      */
-    val rootT = BPlusTreeNode.buildTree(4,
+/*    val rootT = BPlusTreeNode.buildTree(4,
         arrayOf(
             entryArrayOf(
                 1 to ref(6),
@@ -76,7 +75,7 @@ fun main() {
                 9 to ref(7)
             )
         )
-    )
+    )*/
 
 //    val treeT: AbstractBPlusTree = BPlusTreeKotlin(rootT)
 //    println(treeT)
@@ -89,5 +88,34 @@ fun main() {
 //    treeT.insert(8, ref(5))
 //    treeT.insert(1, ref(6))
 //    treeT.insert(9, ref(7))
+
+
+    //val rootT = BPlusTreeNode.buildTree(3, )
+
+/*    val treeT: AbstractBPlusTree = BPlusTreeKotlin(InitialRootNode(3))
+    println(treeT)
+    treeT.insert(1, ref(1))
+    println(treeT)
+    treeT.insert(2, ref(2))
+    println(treeT)
+    treeT.insert(3, ref(3))
+    println(treeT)*/
+
+    val rootT = BPlusTreeNode.buildTree(4,
+        arrayOf(
+            entryArrayOf(
+                2 to ref(1),
+                3 to ref(4)
+            ), entryArrayOf(
+                4 to ref(3),
+                7 to ref(2)
+            )
+        )
+    )
+
+    val treeT: AbstractBPlusTree = BPlusTreeKotlin(rootT)
+    println(treeT)
+    println(treeT.isValid)
+
 
 }
